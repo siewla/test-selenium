13bc0e368f96cdc834fa22e2cfe55c0d62b3feeb
siewla <siewlapang@gmail.com>
2021-02-14 17:38:17 +0800

    manage to do 3 test suites

--- src/test/test.ts
+++ src/test/test.ts
@@ -4,7 +4,7 @@ import 'chromedriver';
 // ThenableWebDriver: A thenable wrapper around an IWebDriver instance
 // that allows commands to be issued directly.
 // Builder: The Builder class is your one-stop-shop for configuring new WebDriver instances.
-import { Builder, ThenableWebDriver } from 'selenium-webdriver';
+import { Builder, By, ThenableWebDriver } from 'selenium-webdriver';
 import * as assert from 'assert';
 
 describe('Suite', () => {
@@ -18,12 +18,74 @@ describe('Suite', () => {
   });
 
   it('Open website and verify it is opened and redirect to login', async () => {
-    await driver.get('https://angular-e2e.herokuapp.com/');
+    await driver.get('http://localhost:4200/login');
     const currentUrl = await driver.getCurrentUrl();
     assert.equal(
-      'https://angular-e2e.herokuapp.com/login',
+      // 'https://angular-e2e.herokuapp.com/login',
+      'http://localhost:4200/login',
       currentUrl,
       'The website is not opened and redirect',
+    );
+  });
+
+  it('key in invalid email and password', async () => {
+    const valid_email = 'wrong';
+    const valid_password = 'wrong';
+
+    const getEmailTextField = await driver.findElement(
+      By.css('input[formControlName=email]'),
+    );
+
+    const getPasswordTextField = await driver.findElement(
+      By.css('input[formControlName=password]'),
+    );
+
+    getEmailTextField.sendKeys(valid_email);
+    getPasswordTextField.sendKeys(valid_password);
+
+    const getLoginButton = await driver.findElement(By.id('login'));
+
+    await getLoginButton.click();
+    const getAlertMsg = await driver
+      .findElement(By.className('alert'))
+      .getText();
+
+    assert.equal(
+      getAlertMsg,
+      'Invalid email or password.',
+      'Wrong credentials keyed in',
+    );
+  });
+
+  it('key in valid email and password', async () => {
+    const valid_email = 'admin@mail.com';
+    const valid_password = 'test123';
+
+    const getEmailTextField = await driver.findElement(
+      By.css('input[formControlName=email]'),
+    );
+
+    const getPasswordTextField = await driver.findElement(
+      By.css('input[formControlName=password]'),
+    );
+
+    await getEmailTextField.clear();
+    await getPasswordTextField.clear();
+
+    getEmailTextField.sendKeys(valid_email);
+    getPasswordTextField.sendKeys(valid_password);
+
+    const getLoginButton = await driver.findElement(By.id('login'));
+
+    await getLoginButton.click();
+
+    const currentUrl = await driver.getCurrentUrl();
+
+    assert.equal(
+      // 'https://angular-e2e.herokuapp.com/login',
+      'http://localhost:4200/',
+      currentUrl,
+      'Valid credentials keyed in and redirect',
     );
   });
 
