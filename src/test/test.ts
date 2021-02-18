6c7f9397246f7f8f8127cfe3ffc8b50b80258a8c
siewla <siewlapang@gmail.com>
2021-02-18 21:37:47 +0800

    add more test cases

--- src/test/test.ts
+++ src/test/test.ts
@@ -4,7 +4,7 @@ import 'chromedriver';
 // ThenableWebDriver: A thenable wrapper around an IWebDriver instance
 // that allows commands to be issued directly.
 // Builder: The Builder class is your one-stop-shop for configuring new WebDriver instances.
-import { Builder, By, ThenableWebDriver } from 'selenium-webdriver';
+import { Builder, By, ThenableWebDriver, until } from 'selenium-webdriver';
 import * as assert from 'assert';
 
 describe('Suite', () => {
@@ -18,11 +18,12 @@ describe('Suite', () => {
   });
 
   it('Open website and verify it is opened and redirect to login', async () => {
-    await driver.get('http://localhost:4200/login');
+    await driver.get('https://angular-e2e.herokuapp.com/login');
+    // await driver.get('http://localhost:4200/login');
     const currentUrl = await driver.getCurrentUrl();
     assert.equal(
-      // 'https://angular-e2e.herokuapp.com/login',
-      'http://localhost:4200/login',
+      'https://angular-e2e.herokuapp.com/login',
+      // 'http://localhost:4200/login',
       currentUrl,
       'The website is not opened and redirect',
     );
@@ -82,8 +83,96 @@ describe('Suite', () => {
     const currentUrl = await driver.getCurrentUrl();
 
     assert.equal(
-      // 'https://angular-e2e.herokuapp.com/login',
-      'http://localhost:4200/',
+      'https://angular-e2e.herokuapp.com/',
+      // 'http://localhost:4200/',
+      currentUrl,
+      'Valid credentials keyed in and redirect',
+    );
+  });
+
+  it('go to register new customer tab', async () => {
+    const getRegisterLink = await driver.findElement(
+      By.xpath("//a[normalize-space()='Register New Customer']"),
+    );
+
+    await getRegisterLink.click();
+
+    const currentUrl = await driver.getCurrentUrl();
+
+    assert.equal(
+      'https://angular-e2e.herokuapp.com/register',
+      // 'http://localhost:4200/register',
+      currentUrl,
+      'Successfuly navigate to register new customer page',
+    );
+  });
+
+  it('register new customer without agent', async () => {
+    const newCustomer = {
+      name: 'New Customer',
+      insuranceName: 'New Insurance',
+      // agentName: 'New Agent',
+      dateActivated: '18/02/2021',
+    };
+
+    const getNameTextField = await driver.findElement(By.css('#name'));
+
+    const getInsuranceNameTextField = await driver.findElement(
+      By.css('#insurance-name'),
+    );
+
+    // const getAgentNameTextField = await driver.findElement(
+    //   By.css('#agent-name'),
+    // );
+
+    const getDateTextField = await driver.findElement(
+      By.css('#date-activated'),
+    );
+
+    getNameTextField.sendKeys(newCustomer.name);
+    getInsuranceNameTextField.sendKeys(newCustomer.insuranceName);
+    // getAgentNameTextField.sendKeys(newCustomer.agentName);
+    getDateTextField.sendKeys(newCustomer.dateActivated);
+
+    const getRegisterButton = await driver.findElement(
+      By.css("button[type='submit']"),
+    );
+
+    await getRegisterButton.click();
+
+    // await driver.wait(until.urlIs('https://angular-e2e.herokuapp.com/'));
+
+    // const currentUrl = await driver.getCurrentUrl();
+
+    const getAlertMsg = await driver
+      .findElement(By.className('alert'))
+      .getText();
+
+    assert.equal(
+      getAlertMsg,
+      'Please fill all fields.',
+      'Got blank field(s) in the form',
+    );
+  });
+
+  it('register new customer with all valid information', async () => {
+    const getAgentNameTextField = await driver.findElement(
+      By.css('#agent-name'),
+    );
+    getAgentNameTextField.sendKeys('new agent');
+
+    const getRegisterButton = await driver.findElement(
+      By.css("button[type='submit']"),
+    );
+
+    await getRegisterButton.click();
+
+    await driver.wait(until.urlIs('https://angular-e2e.herokuapp.com/'));
+
+    const currentUrl = await driver.getCurrentUrl();
+    assert.equal(
+      'https://angular-e2e.herokuapp.com/',
+      // 'http://localhost:4200/',
       currentUrl,
       'Valid credentials keyed in and redirect',
     );
